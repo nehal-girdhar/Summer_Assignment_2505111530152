7caa6d1b6455232a078857d33e8c976d1164d350
NEHAL GIRDHAR <nehalgirdhar9@gmail.com>
2026-06-16 22:24:28 +0530

    Day-16 uploaded

---
+++ Day-16/maximum frequency element.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    int arr[100],n,i,j,count,maxCount=0,element;
+
+    printf("Enter number of elements:");
+    scanf("%d",&n);
+
+    printf("Enter array elements:\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        count=0;
+        for(j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+                count=count+1;
+        }
+        if(count>maxCount)
+        {
+            maxCount=count;
+            element=arr[i];
+        }
+    }
+    printf("Element with maximum frequency=%d\n",element);
+    printf("Frequency=%d",maxCount);
+    return 0;
+}
---
+++ Day-16/missing number in array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int arr[100],n,i,sum=0,total,missing;
+
+    printf("Enter value of n:");
+    scanf("%d",&n);
+
+    printf("Enter %d numbers:\n",n-1);
+
+    for(i=0;i<n-1;i++)
+    {
+        scanf("%d",&arr[i]);
+        sum=sum+arr[i];
+    }
+    total=n*(n+1)/2;
+    missing=total-sum;
+
+    printf("Missing number=%d",missing);
+    return 0;
+}
